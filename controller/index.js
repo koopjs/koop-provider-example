69d64a14a6dbb882fc02bce5aa2259f98b0555c5
chelm <christopher.helm@gmail.com>
2014-10-15 09:37:09 -0600

    new pattern for controller

--- controller/index.js
+++ controller/index.js
@@ -1,12 +1,18 @@
-var BaseController = require('koop-server/lib/Controller.js');
+var BaseController = require('koop-server/lib/BaseController.js');
 
 var Controller = function( Sample ){
 
-  this.index = function(req, res){
+  // inherit from the base controller to share some logic 
+  var controller = {};
+  controller.__proto__ = BaseController();
+
+  // respond to the root route
+  controller.index = function(req, res){
     res.send('This is a sample provider');
   };
 
-  this.get = function(req, res){
+  // get a resource from the providers model 
+  controller.get = function(req, res){
     Sample.find(req.params.id, req.query, function(err, data){
       if (err){
         res.send(err, 500);
@@ -15,8 +21,9 @@ var Controller = function( Sample ){
       }
     });
   };
-
-  this.featureserver = function(req, res){
+  
+  // use the shared code in the BaseController to create a feature service
+  controller.featureserver = function(req, res){
     var callback = req.query.callback, self = this;
     delete req.query.callback;
 
@@ -26,16 +33,19 @@ var Controller = function( Sample ){
       } else {
         // we remove the geometry if the "find" method already handles geo selection in the cache
         delete req.query.geometry;
-        BaseController._processFeatureServer( req, res, err, data, callback);
+        // inherited logic for processing feature service requests 
+        controller.processFeatureServer( req, res, err, data, callback);
       }
     });
   };
 
-  this.preview = function(req, res){
+  // render templates and views 
+  controller.preview = function(req, res){
     res.render(__dirname + '/../views/demo', { locals:{ id: req.params.id } });
   }
   
-  return this;
+  // return the controller so it can be used by koop
+  return controller;
 
 };
 
