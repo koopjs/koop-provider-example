c375501af4a881e8cf46c5c23628e401ceb26c1c
john gravois <jgravois@esri.com>
2017-03-14 14:33:28 -0700

    :nail_care:

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 /*
   routes.js
 
-  This file is an optional place to specify additional route to be handled by this provider's controller
+  This file is an optional place to specify additional routes to be handled by this provider's controller
   Documentation: http://koopjs.github.io/docs/specs/provider/
 */
 module.exports = []
