77ce7c99cf52d807fa268503007d1ffb286595c7
Daniel Fenton <dfenton@esri.com>
2017-03-15 18:18:11 -0400

    correct function signature in model-test

--- test/model-test.js
+++ test/model-test.js
@@ -15,7 +15,7 @@ test('should properly fetch from the API and translate features', t => {
   .get('/ws/v2/vehicles/onRouteOnly/false/appid/8A0EBB788E8205888807BAC97')
   .reply(200, require('./fixtures/input.json'))
 
-  model.getData((err, geojson) => {
+  model.getData({}, (err, geojson) => {
     t.error(err)
     t.equal(geojson.type, 'FeatureCollection', 'creates a feature collection object')
     t.ok(geojson.features, 'has features')
