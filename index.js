599ebd52bc8c34c2b9e288a827868542c29d6259
ajturner <aturner@esri.com>
2017-09-02 13:44:27 -0400

    Added get started and guidance docs

--- index.js
+++ index.js
@@ -6,11 +6,14 @@
   Documentation: http://koopjs.github.io/docs/specs/provider/
 */
 
+// Define the provider path
+// /:name/:hosts?/:disableIdParam?/FeatureServer/:layer/:method
+// e.g. /sample/FeatureServer/0/query
 const provider = {
   type: 'provider',
   name: 'sample',
-  hosts: false,
-  disableIdParam: true,
+  hosts: false, // if true, also adds disableIdParam
+  disableIdParam: true, // if true, adds to path and req.params
   Controller: require('./controller'),
   Model: require('./model'),
   routes: require('./routes'),
