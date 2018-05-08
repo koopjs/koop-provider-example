3500557a30e1f069120bb0ce7721c3ac4e0d23ed
Rich Gwozdz <rich.gwozdz@gmail.com>
2018-05-08 14:20:50 -0700

    Remove local koop require.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ process.on('SIGINT', () => process.exit(0))
 process.on('SIGTERM', () => process.exit(0))
 
 // Initialize Koop
-const Koop = require('./koop-core/src')
+const Koop = require('koop')
 const koop = new Koop()
 
 // Install Craigslist Provider
