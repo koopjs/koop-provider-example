0889467bb7fa2bf5d8644da7e26731de145d522c
Zack Beatty <zack@seeclickfix.com>
2018-12-18 13:51:00 -0800

    Fix broken links to Koop Provider docs (#26)
    And yes, the new URL only works without the trailing slash. :/

--- controller.js
+++ controller.js
@@ -4,7 +4,7 @@
   This file is not required unless additional routes specified in routes.js
   If so, corresponding functions must be written to match those routes.
 
-  Documentation: http://koopjs.github.io/docs/specs/provider/
+  Documentation: http://koopjs.github.io/docs/usage/provider
 */
 
 function Controller (model) {
