243df2a7feb6c8dec2928623bfc590ef3872d60a
Christina Mosnick <cmosnick07@gmail.com>
2019-03-10 23:23:06 -0500

    Updated server start message to include provider name (#28)

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ if (process.env.DEPLOY === 'export') {
   For more docs visit: https://koopjs.github.io/docs/usage/provider
   To find providers visit: https://www.npmjs.com/search?q=koop+provider
 
-  Try it out in your browser: http://localhost:${port}/sample/FeatureServer/0/query
-  Or on the command line: curl --silent http://localhost:${port}/sample/FeatureServer/0/query?returnCountOnly=true
+  Try it out in your browser: http://localhost:${port}/${provider.name}/FeatureServer/0/query
+  Or on the command line: curl --silent http://localhost:${port}/${provider.name}/FeatureServer/0/query?returnCountOnly=true
 
   Press control + c to exit
   `
